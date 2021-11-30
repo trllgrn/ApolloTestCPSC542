66efee74a337180cc131be40d4fc1eab74a94361
Randall Lee <rleemse@csu.fullerton.edu>
2021-11-30 10:57:29 -0800

    Updated Test Header.tsx
    Used Sazeda's Test Footer.tsx as a model

--- client/src/components/__tests__/header.tsx
+++ client/src/components/__tests__/header.tsx
@@ -3,11 +3,23 @@ import React from 'react';
 import { render, cleanup } from '../../test-utils';
 import Header from '../header';
 
+import { MenuItem } from '..';
+import { ReactWrapper } from 'enzyme';
+
+MenuItem.displayName = "MenuItem"
+
 describe('Header', () => {
+    let wrapper: ReactWrapper
+    beforeEach(() => {
+        wrapper = renderApollo(<Header />)
+    })
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it('renders without error', () => {
-    render(<Header />);
+    it('Check Components are rendered as expected', () => {
+    //render(<Header />);
+
+        expect(wrapper.find("InnerContainer").length).toBe(1)
+        expect(wrapper.find("MenuItem").length).toBe(3)
   });
 });
