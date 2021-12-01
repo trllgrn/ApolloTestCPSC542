a483bb00199a106b0f3d6e405cf941869cfa1ae5
Randall Lee <rleemse@csu.fullerton.edu>
2021-11-30 17:37:49 -0800

    Action Button

--- client/src/containers/__tests__/action-button.tsx
+++ client/src/containers/__tests__/action-button.tsx
@@ -13,17 +13,18 @@ describe('action button', () => {
     expect(getByTestId('action-button')).toBeTruthy();
   });
 
+    // add
   it('shows correct label', () => {
     const { getByText, container } = renderApollo(<ActionButton />);
     getByText(/add to cart/i);
 
-    // rerender with different props to same container
+    // rerender with different props to same container remove
     cartItemsVar(['1']);
     renderApollo(<ActionButton id="1" />, { container });
     getByText(/remove from cart/i);
     cartItemsVar([]);
 
-    // rerender with different props to same container
+    // rerender with different props to same container cancel
     renderApollo(<ActionButton isBooked={true} />, { container });
     getByText(/cancel this trip/i);
   });
