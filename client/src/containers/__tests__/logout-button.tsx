f50ef0020342ca27b5b767d10e1a791e04b84100
bktran8 <33641377+bktran8@users.noreply.github.com>
2021-12-05 13:46:39 -0800

    Revert "Joe"

--- client/src/containers/__tests__/logout-button.tsx
+++ client/src/containers/__tests__/logout-button.tsx
@@ -3,32 +3,21 @@ import LogoutButton from '../logout-button';
 
 import { renderApollo, cleanup, fireEvent } from '../../test-utils';
 import { cache, isLoggedInVar } from '../../cache';
-import { MockedProvider } from '@apollo/client/testing';
-import { ApolloProvider, useApolloClient } from '@apollo/client';
-import { shallow, render, mount } from 'enzyme'
 
 describe('logout button', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
   it('renders logout button', async () => {
-    const wrapper = render(
-      <MockedProvider  addTypename={false}>
-          <LogoutButton />
-      </MockedProvider>
-      );
+    renderApollo(<LogoutButton />);
   });
 
   it('complete logout', async () => {
     isLoggedInVar(true);
     localStorage.setItem('token', 'testTokenValue');
     localStorage.setItem('userId', 'abc123');
-
-    //Use enzyme render for button
-    const wrapper = shallow(<LogoutButton />);
-    wrapper.find({ "data-testid": "logout-button" }).simulate("click");
-    
-
+    const { getByTestId } = renderApollo(<LogoutButton />, { cache });
+    fireEvent.click(getByTestId('logout-button'));
     expect(isLoggedInVar()).toBeFalsy();
     expect(localStorage.getItem('token')).toBeNull();
     expect(localStorage.getItem('userId')).toBeNull();
